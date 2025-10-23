6df6945e829f72c7576a74cb3358960ff0fec871
Vlaani <av.anisimov.9@gmail.com>
2025-10-23 11:44:22 +0300

    Доделана практическая работа

--- students/k3340/practical_works/Anisimov_Vladislav/simple_django_web_project/project_first_app/forms.py
+++ students/k3340/practical_works/Anisimov_Vladislav/simple_django_web_project/project_first_app/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from .models import Owner, Car
+from .models import Owner, Car, Ownership
 
 class OwnerForm(forms.ModelForm):
     class Meta:
@@ -15,3 +15,21 @@ class CarForm(forms.ModelForm):
     class Meta:
         model = Car
         fields = ['license_plate', 'brand', 'model', 'color']
+
+class OwnershipForm(forms.ModelForm):
+    owner = forms.ModelChoiceField(
+        queryset=Owner.objects.all(),
+        widget=forms.Select(attrs={'class': 'form-control'})
+    )
+    car = forms.ModelChoiceField(
+        queryset=Car.objects.all(),
+        widget=forms.Select(attrs={'class': 'form-control'})
+    )
+    
+    class Meta:
+        model = Ownership
+        fields = ['owner', 'car', 'start_date', 'end_date']
+        widgets = {
+            'start_date': forms.DateInput(attrs={'class': 'form-control', 'type': 'date'}),
+            'end_date': forms.DateInput(attrs={'class': 'form-control', 'type': 'date'}),
+        }
--- students/k3340/practical_works/Anisimov_Vladislav/simple_django_web_project/project_first_app/views.py
+++ students/k3340/practical_works/Anisimov_Vladislav/simple_django_web_project/project_first_app/views.py
@@ -2,8 +2,8 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.http import Http404
 from django.urls import reverse_lazy
 from django.views.generic import ListView, DetailView, UpdateView, CreateView, DeleteView
-from .forms import OwnerForm, CarForm
-from .models import Car, Owner
+from .forms import OwnerForm, CarForm, OwnershipForm
+from .models import Car, Owner, Ownership
 
 def owner_detail(request, id):
     owner = get_object_or_404(Owner, id=id)
@@ -19,7 +19,9 @@ def car_list(request):
 
 def car_detail(request, id):
     car = get_object_or_404(Car, id=id)
-    return render(request, 'car_detail.html', {'car': car})
+    owners = Ownership.objects.filter(car=car)
+    last_owner = max(owners, key=lambda ownership: ownership.start_date).owner
+    return render(request, 'car.html', {'car': car, 'owner': last_owner, 'ownership': owners})
 
 def car_update(request, id):
     car = get_object_or_404(Car, id=id)
@@ -48,3 +50,35 @@ def owner_create(request):
     else:
         form = OwnerForm()
     return render(request, 'owner_form.html', {'form': form, 'title': 'Добавить человека'})
+
+def add_car(request):
+    if request.method == 'POST':
+        form = CarForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('car_list')
+    else:
+        form = CarForm()
+    
+    return render(request, 'add_car.html', {'form': form})
+
+def add_ownership(request):
+    if request.method == 'POST':
+        form = OwnershipForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('ownership_list')
+    else:
+        form = OwnershipForm()
+    
+    return render(request, 'add_ownership.html', {'form': form})
+
+def ownership_list(request):
+    ownerships = Ownership.objects.select_related('owner', 'car').all()
+    return render(request, 'ownership_list.html', {'ownerships': ownerships})
+
+def ownership_detail(request, id):
+    car = get_object_or_404(Car, id=id)
+    owners = Ownership.objects.filter(car=car)
+    ownerships = owners.select_related('owner', 'car').all()
+    return render(request, 'ownership_list.html', {'ownerships': ownerships})
